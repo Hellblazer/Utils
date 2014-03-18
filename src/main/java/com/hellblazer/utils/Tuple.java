8df448d5ac0218b7c226df81aae47a9c28a01132
Hellblazer <hellblazer@me.com>
2014-03-18 15:56:25 -0700

    Add a fucking Tuple.  Geebus, Oracle.  Really?

---
+++ src/main/java/com/hellblazer/utils/Tuple.java
@@ -0,0 +1,41 @@
+/** (C) Copyright 2013 Hal Hildebrand, All Rights Reserved
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package com.hellblazer.utils;
+
+/**
+ * It pains me that this is not a part of the base JDK.
+ * 
+ * @author hhildebrand
+ * 
+ */
+public class Tuple<A, B> {
+    public final A a;
+    public final B b;
+
+    public Tuple(A a, B b) {
+        super();
+        this.a = a;
+        this.b = b;
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+}
